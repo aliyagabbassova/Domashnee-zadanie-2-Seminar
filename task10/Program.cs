9fadb1a10e8579cce895f8df490ce5e245fe6db8
aliya <aliya.gabbassova1911@gmail.com>
2022-11-29 16:04:43 +0600

    Задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Input three-digit number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int count;
+count = num % 100 /10;
+Console.WriteLine($"The second digit of {num} is {count}");
